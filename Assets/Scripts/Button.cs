1eadf9294c146b06985ef080a58bf9639e514f9d
Lauren Hetherington <hetherington@astro.utoronto.ca>
2018-05-02 16:22:24 -0400

    Added a button operated door

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+    public GameObject PushMe;
+    public GameObject Door;
+    ButtonDoor doorscript;
+    Material ButtonLight;
+    bool ison;
+    //bool permaon;
+	// Use this for initialization
+	void Start () {
+        ButtonLight = PushMe.GetComponent<Renderer>().material;
+        doorscript = Door.GetComponent<ButtonDoor>();
+	}
+
+    public bool IsOn() {
+        return ison;
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+        ison = true;
+        doorscript.AddButton();
+        ButtonLight.EnableKeyword("_EMISSION");
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+        ison = false;
+        doorscript.AddButton(-1);
+        ButtonLight.DisableKeyword("_EMISSION");
+	}
+}
---
+++ Assets/Scripts/ButtonDoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDoor : MonoBehaviour {
+
+    // Use this for initialization
+    public int ButtonsNeeded;
+    public float OpenHeight;
+    int buttons;
+    float height;
+    Rigidbody rb;
+	void Start () {
+        buttons = 0;
+        height = 0;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (buttons>=ButtonsNeeded && height<OpenHeight) {
+            height += Time.deltaTime;
+            rb.MovePosition(rb.position + Vector3.up * Time.deltaTime);
+        }
+        else if (buttons<ButtonsNeeded && height>0) {
+            height -= 3*Time.deltaTime;
+            rb.MovePosition(rb.position - 3*Vector3.up * Time.deltaTime);
+        }
+	}
+
+    public void AddButton(int addone=1) {
+        buttons += addone;
+    }
+}
